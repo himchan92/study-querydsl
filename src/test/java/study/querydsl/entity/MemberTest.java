84fc511832548625ff68f88f65115923305c047b
himchan92 <omea92@naver.com>
2025-08-28 12:26:20 +0900

    Query setting test

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,43 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+
+        em.flush();
+        em.clear();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+
+        for(Member member : members) {
+            System.out.println(member.getUsername());
+        }
+    }
+
+}
